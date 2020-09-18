7d2dd6fcd0c217cde3bfb5da8167fcdb1622e5e4
Ralf Lang <lang@b1-systems.de>
2020-09-18 21:37:02 +0000

    Override methods which are likely to spill an injector instance without a non-namespaced class ontop

--- lib/Horde/Injector.php
+++ lib/Horde/Injector.php
@@ -30,4 +30,27 @@ use Horde\Injector\Injector;
  */
 class Horde_Injector extends Injector implements \Horde_Injector_Scope
 {
+
+    public function __construct(\Horde_Injector_Scope $scope)
+    {
+        parent::__construct($scope);
+        $this->setInstance('Horde_Injector', $this);
+        $this->setInstance('\Horde_Injector', $this);
+    }
+
+    /**
+     * Create a child injector that inherits this injector's scope.
+     *
+     * All child injectors inherit the parent scope.  Any objects that were
+     * created using getInstance, will be available to the child container.
+     * The child container can set bindings to override the parent, and none
+     * of those bindings will leak to the parent.
+     *
+     * @return Injector  A child injector with $this as its parent.
+     */
+    public function createChildInjector(): Injector
+    {
+        // Using self is wrong and breaks wrapping into inheriting injectors
+        return new self($this);
+    }
 }
